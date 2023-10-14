3b0268016a94027427314bb52e9e0a22e02899fd
viadel13 <shababizetre@gmail.com>
2023-10-14 22:03:18 +0100

    first commit

--- src/components/Sidebar/menus/ListeEnvois.js
+++ src/components/Sidebar/menus/ListeEnvois.js
@@ -1,49 +1,88 @@
-import { useSelector } from "react-redux/es/hooks/useSelector";
+import { useEffect, useState } from "react";
+import { db } from "../../../firebase/firebaseConfig";
+import { collection, query, onSnapshot } from "firebase/firestore";
 
 const ListeEnvois = () => {
-
   // const datasEnvoi = useSelector((state)=> state.platformeSuivi.datasEnvoi);
-  const datasEnvoi = JSON.parse(localStorage.getItem('datasEnvoi'));
-  
-  const tableDatas = datasEnvoi && datasEnvoi.map((i, index)=>{
-    return(
+  // const datasEnvoi = JSON.parse(localStorage.getItem('datasEnvoi'));
+  const [donneesEnvoi, setDonneesEnvoi] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const q = query(collection(db, "DatasEnvoi"));
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(q, (querySnapchot) => {
+      const datas = [];
+      querySnapchot.forEach((doc) => {
+        datas.push(doc.data());
+      });
+
+      setDonneesEnvoi(datas);
+      setLoading(false);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, [q]);
+
+  const tableDatas = donneesEnvoi.map((i, index) => {
+    return (
       <tr key={index}>
         <td>{i.numeroSuivi}</td>
         <td>{i.nomProduit}</td>
         <td>{i.quantite}</td>
         <td>{i.categorie}</td>
-        <td>En livraison</td>
+        <td style={{width: '290px'}}>
+          <select className="form-select" aria-label="Default select example" style={{ border: 'none'}}>
+            <option value="1">En attente de dédouanement</option>
+            <option value="2">En transit </option>
+            <option value="3">En livraison</option>
+            <option value="4">Livré </option>
+          </select>
+        </td>
       </tr>
-    )
-  })
+    );
+  });
 
-return (
-<div className="container mt-5">
-<div style={{backgroundColor: 'white'}}>
-    <h2>Liste des envois</h2>
+  return (
+    <div className="container mt-5">
+      <div style={{ backgroundColor: "white" }}>
+        <h2>Liste des envois</h2>
+      </div>
+      <div className="table-responsive">
+        <table className="table table-bordered">
+          <thead className="table-primary">
+            <tr>
+              <th>Numéro de suivi</th>
+              <th>Nom du produit</th>
+              <th>Quantité</th>
+              <th>Catégorie</th>
+              <th>État</th>
+            </tr>
+          </thead>
+          <tbody>
+            {loading ? (
+              <tr>
+                <td colSpan="5" className="text-center p-4">
+                  <div className="spinner-border text-primary" role="status">
+                    <span className="visually-hidden">Loading...</span>
+                  </div>
+                </td>
+              </tr>
+            ) : tableDatas.length !== 0 ? (
+              tableDatas
+            ) : (
+              <tr>
+                <td colSpan="5" className="text-center p-4">
+                  Aucune donnée
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
     </div>
-    <div className='table-responsive'>
-    <table className="table table-bordered">
-      <thead className='table-primary'>
-        <tr>
-          <th>Numéro de suivi</th>
-          <th>Nom du produit</th>
-          <th>Quantité</th>
-          <th>Catégorie</th>
-          <th>État</th>
-        </tr>
-      </thead>
-      <tbody>
-        {tableDatas}
-      </tbody>
-    </table>
-    {
-      !datasEnvoi && <p className="text-center">Aucune donnee</p>
-    }
-
-    </div>
-  </div>
-  )
-}
+  );
+};
 
 export default ListeEnvois;
--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -3,24 +3,8 @@ import { collection, getDocs } from "firebase/firestore";
 import { db } from "../../firebase/firebaseConfig";
 
 
-
-
-const fetchDataFromFirestore = async () =>{
-    const querySnapshot = await getDocs(collection(db, "DatasEnvoi"));
-    const data = [];
-    querySnapshot.forEach((doc)=>{
-        data.push(doc.data());
-    })
-
-    localStorage.setItem("datasEnvoi", JSON.stringify(data));
-}
-
-fetchDataFromFirestore();
-
-const localStorageDatas = JSON.parse(localStorage.getItem('datasEnvoi')) || [] ;
-
 const initialState = {
-    datasEnvoi: localStorageDatas,
+    datasEnvoi: [],
 }
 
 const platformeSuiviSlice = createSlice({
@@ -31,7 +15,6 @@ const platformeSuiviSlice = createSlice({
         
         datasEnvoi: (state, action)=>{
             state.datasEnvoi = [...state.datasEnvoi, action.payload];
-            localStorage.setItem("datasEnvoi", JSON.stringify(state.datasEnvoi));
         },
 
     }
