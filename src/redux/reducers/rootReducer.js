734b944f5870a86f51c1fa720e68149333faa4bb
viadel13 <shababizetre@gmail.com>
2023-11-19 11:53:21 +0100

    first commit

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -1,31 +1,60 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { collection, addDoc, onSnapshot, query } from "firebase/firestore";
+import { db } from "../../firebase/firebaseConfig";
 
 const initialState = {
     datasEnvoi: [],
     toggleSidebar: '',
     modalEtat: true,
+    datasDoc: [],
 }
 
 const platformeSuiviSlice = createSlice({
     name: 'platformeSuivi',
     initialState,
 
-    reducers:{
-        
-        datasEnvoi: (state, action)=>{
+    reducers: {
+
+        datasEnvoi: (state, action) => {
             state.datasEnvoi = [...state.datasEnvoi, action.payload];
         },
-        toggleSide: (state, action)=>{
+        toggleSide: (state, action) => {
             state.toggleSidebar = action.payload;
         },
-        modalEtat: (state, action)=>{
+        modalEtat: (state, action) => {
             state.modalEtat = action.payload;
         },
+        datasDoc: (state, action) => {
+            state.datasDoc = [...state.datasDoc, action.payload]
+        }
 
     }
 })
 
-export const {datasEnvoi, toggleSide, modalEtat} = platformeSuiviSlice.actions;
+export const { datasEnvoi, toggleSide, modalEtat, datasDoc } = platformeSuiviSlice.actions;
 
+export const addDocument = (link) => async (dispacth) => {
+    try {
+
+        await addDoc(collection(db, "Documents"), {
+            NumeroSuivi: '',
+            ConnaissementBL: '',
+            numeroFacture: '',
+            listeColissage: link[0] || '',
+            certificatOrigine: link[1] || '',
+            certificatPhytoSanitaire: link[2] || '',
+            RCV: '',
+            fichierRCV: link[3] || '',  // Assurez-vous que link[3] n'est pas undefined
+            PAD: '',
+            FichierPAD: link[4] || '',
+            autorisationEnlevement: link[5] || '',
+            bonSortie: link[6] || '',
+            Autres: link[7] || '',
+        });
+
+    } catch (error) {
+        console.log(error);
+    }
+}
 
 export default platformeSuiviSlice.reducer;
